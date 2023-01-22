89bf3411c730754a8c3230a87293ae840d80954c
SHumona-Akter <shumona.cse.bd@gmail.com>
2023-01-22 13:19:24 +0600

    some change

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,30 @@ module.exports = {
     },
     plugins: [],
   }
-  
+  <section id="shopping" class="mb-24 md:grid grid-cols-2 gap-6 justify-center  items-center">
+          <div>
+            
+          </div>
+          <div class="md:flex place-items-center items-center justify-between bg-white rounded-md shadow-md p-5 mb-4">
+            <div>
+              <img src="./images/box.png" alt="" class="w-1/3">
+            </div>
+            <div class="text-center md:text-left">
+              <h1 class="text-2xl text-sondary mt-3 mb-3 font-bold">Find the Perfect Fit</h1>
+              <p class="text-base text-dark leading-7">Everybody is different, which is why we
+                offer styles for every body.</p>
+            </div>
+        </div>
+        <div class="md:flex place-items-center items-center justify-between bg-white rounded-md shadow-md p-5 mb-4">
+          <div class="ml-6">
+            <img src="./images/question.png" alt="" class="w-1/3">
+          </div>
+          <div class="text-center md:text-left">
+            <h1 class="text-2xl text-sondary mt-3 mb-3 font-bold">Find the Perfect Fit</h1>
+            <p class="text-base text-dark leading-7">Everybody is different, which is why we
+              offer styles for every body.</p>
+          </div>
+      </div>
+        </div>
+        </div>
+      </section>
